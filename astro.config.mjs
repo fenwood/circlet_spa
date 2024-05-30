b0be49e850478de46245e5156569c4085d084174
Jeremy Thompson <fenwood@gmail.com>
2024-05-29 20:27:18 -0400

    Added cloudflare adapter for SSR (dynamice urls for recipe pages)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,15 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig, passthroughImageService } from "astro/config";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-moon-landing.netlify.app/",
   integrations: [tailwind(), icon()],
   output: 'server',
-  image: { 
-     service: passthroughImageService(),
+  image: {
+    service: passthroughImageService()
   },
+  adapter: cloudflare()
 });
